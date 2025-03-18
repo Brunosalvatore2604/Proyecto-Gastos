e4c4229d726769aabbe8fc335970f0a926f18745
Brunosalvatore2604 <143473123+Brunosalvatore2604@users.noreply.github.com>
2025-03-18 20:45:23 +0000

    deploy 2

--- back/app.js
+++ back/app.js
@@ -46,7 +46,7 @@ app.post("/login", (req, res) => {
 
 const verificarToken = (req,res,next)=>{
 
-    const token = req.headers["Authorization"]?.split(" ")[1];
+    const token = req.headers["authorization"]?.split(" ")[1];
 
     if(!token) return res.status(401).json({mensaje:"Token invalido"});
 
@@ -57,7 +57,7 @@ const verificarToken = (req,res,next)=>{
     });
 }
 
-app.get("/main",verificarToken(),(req,res)=>{
+app.get("/main",verificarToken,(req,res)=>{
     res.json({mensjae:`Bienvenido ${req.usuario.nombre}`});
 });
 
