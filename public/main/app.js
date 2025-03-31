81333ff179987a840c5f43b6e104708218d301f4
Brunosalvatore2604 <143473123+Brunosalvatore2604@users.noreply.github.com>
2025-03-31 15:44:54 +0000

    Creacion de tabla Pago v0.1

--- public/main/app.js
+++ public/main/app.js
@@ -354,7 +354,7 @@ document.addEventListener("click", e=>{
 // Getar Gastos por Grupo
 document.addEventListener("click", async e => {
 
-    if(e.target.id == "gasto-nuevo"){
+    if(e.target.id == "nuevo-gasto"){
         return;
     }
 
